cb288058711eb93b08ffee2aa19641fbc0ad6aa9
Brittney <brittfort34@gmail.com>
2018-09-20 09:25:07 +0000

    ES6 Promises

--- test/connection.js
+++ test/connection.js
@@ -1,13 +1,19 @@
 const mongoose = require('mongoose');
 
-// connect to mongodb
-mongoose.connect('mongodb://localhost/testaroo', { useNewUrlParser: true } );
+// ES6 Promises
+mongoose.Promise = global.Promise;
 
-mongoose.connection.once('open', function(){
-   console.log('Connection has been made, now make fireworks...'); 
-}).on('error', function(error){
-    console.log('Connection error: ', error);
+// Connect to the database before tests run
+before(function(done) {
+    // connect to mongodb
+    mongoose.connect('mongodb://localhost/testaroo', { useNewUrlParser: true });
+
+    mongoose.connection.once('open', function() {
+        console.log('Connection has been made, now make fireworks...');
+        done();
+    }).on('error', function(error) {
+        console.log('Connection error: ', error);
+    });
 });
-
 
 
