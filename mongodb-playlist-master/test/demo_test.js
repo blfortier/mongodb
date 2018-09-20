427f39baac5fa3cbe1537a8dd68a48a4be7ba954
Brittney <brittfort34@gmail.com>
2018-09-20 08:34:00 +0000

    connecting to mongodb

---
+++ mongodb-playlist-master/test/demo_test.js
@@ -0,0 +1,13 @@
+const mocha = require('mocha');
+const assert = require('assert');
+
+
+describe('some demo test', function(){
+    
+    // Create tests
+    it('adds two numbers together', function(){
+        assert(2 + 3 === 5);
+    });
+    
+});
+
