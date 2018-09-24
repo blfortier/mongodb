8d125f85ec85c1cac66e552e93c094f5d9b5503c
Brittney <brittfort34@gmail.com>
2018-09-24 16:18:28 +0000

    Object ID

--- test/finding_test.js
+++ test/finding_test.js
@@ -4,8 +4,10 @@ const MarioChar = require('../models/mariochar');
 // Describe tests
 describe('Finding records', function(){
   
+    var char;
+    
     beforeEach(function(done){
-         var char = new MarioChar({
+            char = new MarioChar({
             name: 'Mario'
         });
         
@@ -21,6 +23,15 @@ describe('Finding records', function(){
         
        MarioChar.findOne({name: 'Mario'}).then(function(result){
            assert(result.name === 'Mario');
+           done();
+       });
+       
+    });
+    
+    it('Finds one record by ID from the database', function(done){
+        
+       MarioChar.findOne({_id: char._id}).then(function(result){
+           assert(result._id.toString() === char._id.toString());
            done();
        });
        
